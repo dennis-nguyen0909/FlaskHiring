da03efb495bfe38e13e15572c287e4c13f79d39b
Minh Duy <dennis.nguyen0909@gmail.com>
2025-04-09 23:26:48 +0700

    test: ci/cd

--- app.py
+++ app.py
@@ -357,6 +357,6 @@ def analyze():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
+# test
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5001, debug=True) 
